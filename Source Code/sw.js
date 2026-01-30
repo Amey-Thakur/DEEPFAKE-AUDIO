22ff0f3f8b21987cf0a935e214f94523f5e13c97
Amey Thakur <ameythakur20@gmail.com>
2026-01-29 21:33:26 -0500

    Deepfake-Audio
    Co-authored-by: Mega Satish <mega.modha@gmail.com>

---
+++ Source Code/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'deepfake-audio-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/file=favicon.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
